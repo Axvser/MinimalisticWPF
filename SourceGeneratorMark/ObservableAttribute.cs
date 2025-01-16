405eebc8db911bd1dfd35f4b3c6e7dc23d0d1a3a
冯 <2789083329@qq.com>
2025-01-16 16:32:21 +0800

    将回收触发部分移交给Observable可选参数描述

--- SourceGeneratorMark/ObservableAttribute.cs
+++ SourceGeneratorMark/ObservableAttribute.cs
@@ -16,13 +16,15 @@ namespace MinimalisticWPF
     /// <param name="SetterValidation">Select how the property is validated when updates occur</param>
     /// <param name="CanOverride">Select whether the property is overwritable</param>
     /// <param name="CanHover">Select whether to generate hover properties.If enabled, an initial value must be set for the field, and this initial value will affect the initial value set for the dependent property</param>
+    /// <param name="CanInvokeRelease">Select whether to notify the object pool of attempting to recycle elements when the Setter triggers</param>
     /// <param name="Cascades">Changes to this property are shared with other properties</param>
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
-    public class ObservableAttribute(SetterValidations SetterValidation = SetterValidations.Compare, bool CanOverride = false, bool CanHover = false, params string[] Cascades) : Attribute
+    public class ObservableAttribute(SetterValidations SetterValidation = SetterValidations.Compare, bool CanOverride = false, bool CanHover = false, bool CanInvokeRelease = false, params string[] Cascades) : Attribute
     {
-        public SetterValidations SetterValidation { get; private set; } = SetterValidation; 
+        public SetterValidations SetterValidation { get; private set; } = SetterValidation;
         public bool CanOverride { get; private set; } = CanOverride;
         public bool CanHover { get; private set; } = CanHover;
+        public bool CanInvokeRelease { get; private set; } = CanInvokeRelease;
         public string[] Cascades { get; private set; } = Cascades;
     }
 }
