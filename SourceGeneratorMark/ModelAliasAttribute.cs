01a20df7f7e5a6c7d4c0c5bba02543d9ad8f1315
俊杰 冯 <Axvser@outlook.com>
2025-03-25 13:38:12 +0800

    引入Model输出自动化

---
+++ SourceGeneratorMark/ModelAliasAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimalisticWPF
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class ModelAliasAttribute(string alias) : Attribute
+    {
+        public string Alias { get; internal set; } = alias;
+    }
+}
---
+++ SourceGeneratorMark/ModelConfigAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimalisticWPF
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class ModelConfigAttribute(string modelName, string nameSpace = "") : Attribute
+    {
+        public string ModelName { get; internal set; } = modelName;
+        public string NameSpace { get; internal set; } = nameSpace;
+    }
+}
