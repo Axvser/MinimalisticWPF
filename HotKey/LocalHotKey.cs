bc73e53ebc672b35f5a99d74f18a4ec2000e3d16
俊杰 冯 <Axvser@outlook.com>
2025-05-15 13:42:38 +0800

    + 使用弱引用优化局部热键的缓存

--- HotKey/LocalHotKey.cs
+++ HotKey/LocalHotKey.cs
@@ -1,77 +1,43 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Input;
 
 namespace MinimalisticWPF.HotKey
 {
-    /// <summary>
-    /// 🧰 > Local hotkey registration
-    /// <para>Core</para>
-    /// <para>- <see cref="Register"/></para>
-    /// <para>- <see cref="Unregister(UIElement,HashSet{Key})"/></para>
-    /// <para>- <see cref="Unregister(UIElement)"/></para>
-    /// </summary>
     public static class LocalHotKey
     {
-        public static void Register(UIElement target, HashSet<Key> keys, KeyEventHandler keyevent)
+        private static readonly ConditionalWeakTable<UIElement, HashSet<LocalHotKeyInjector>> _injectors = new();
+
+        public static void Register(UIElement target, HashSet<Key> keys, KeyEventHandler handler)
         {
-            var injector = new LocalHotKeyInjector(target, keys, keyevent);
-            if (LocalHotKeyInjector.Injectors.TryGetValue(target, out var injectorSet))
-            {
-                injectorSet.Add(injector);
-            }
-            else
-            {
-                LocalHotKeyInjector.Injectors.Add(target, [injector]);
-            }
+            if (target == null) throw new ArgumentNullException(nameof(target));
+
+            var injector = new LocalHotKeyInjector(target, keys, handler);
+            var injectorSet = _injectors.GetOrCreateValue(target);
+            injectorSet.Add(injector);
         }
         public static int Unregister(UIElement target, HashSet<Key> keys)
         {
-            int count = 0;
-
-            if (LocalHotKeyInjector.Injectors.TryGetValue(target, out var injectorSet))
+            if (_injectors.TryGetValue(target, out var injectorSet))
             {
-                List<LocalHotKeyInjector> removed = [];
-                foreach (var injector in injectorSet)
-                {
-                    if (keys.IsSupersetOf(injector._targetKeys))
-                    {
-                        target.RemoveHandler(UIElement.PreviewKeyDownEvent, injector.Receiver);
-                        target.RemoveHandler(UIElement.PreviewKeyUpEvent, injector.ReleaseReceiver);
-                        target.RemoveHandler(UIElement.LostFocusEvent, injector.ClearValues);
-                        removed.Add(injector);
-                    }
-                }
-
-                foreach (var injector in removed)
-                {
-                    if (injectorSet.Remove(injector))
-                    {
-                        count++;
-                    }
-                }
-
-                if (!injectorSet.Any()) LocalHotKeyInjector.Injectors.Remove(target);
+                int removed = injectorSet.RemoveWhere(i => i._targetKeys.SetEquals(keys));
+                if (injectorSet.Count == 0) _injectors.Remove(target);
+                return removed;
             }
-
-            return count;
+            return 0;
         }
         public static int Unregister(UIElement target)
         {
-            int count = 0;
-
-            if (LocalHotKeyInjector.Injectors.TryGetValue(target, out var injectorSet))
+            if (_injectors.TryGetValue(target, out var injectorSet))
             {
-                foreach (var injector in injectorSet)
-                {
-                    target.RemoveHandler(UIElement.PreviewKeyDownEvent, injector.Receiver);
-                    target.RemoveHandler(UIElement.PreviewKeyUpEvent, injector.ReleaseReceiver);
-                    target.RemoveHandler(UIElement.LostFocusEvent, injector.ClearValues);
-                    count++;
-                }
-                LocalHotKeyInjector.Injectors.Remove(target);
+                int count = injectorSet.Count;
+                foreach (var injector in injectorSet) injector.Dispose();
+                _injectors.Remove(target);
+                return count;
             }
-
-            return count;
+            return 0;
         }
     }
 }
--- HotKey/LocalHotKeyInjector.cs
+++ HotKey/LocalHotKeyInjector.cs
@@ -1,54 +1,74 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Input;
 
 namespace MinimalisticWPF.HotKey
 {
-    internal class LocalHotKeyInjector
+    internal sealed class LocalHotKeyInjector : IDisposable
     {
-        internal static readonly Dictionary<UIElement, HashSet<LocalHotKeyInjector>> Injectors = [];
+        internal readonly WeakReference<UIElement> _targetWeakRef;
+        internal readonly HashSet<Key> _pressedKeys = new();
+        internal readonly HashSet<Key> _targetKeys;
+        internal readonly KeyEventHandler _keyEventHandler;
 
-        internal UIElement _target;
-
-        internal HashSet<Key> _pressedKeys = [];
-
-        internal HashSet<Key> _targetKeys = [];
-
-        internal KeyEventHandler keyEventHandler;
-
-        internal LocalHotKeyInjector(UIElement target, HashSet<Key> keys, KeyEventHandler keyevent)
+        internal LocalHotKeyInjector(UIElement target, HashSet<Key> keys, KeyEventHandler handler)
         {
-            _target = target;
+            _targetWeakRef = new WeakReference<UIElement>(target);
             _targetKeys = keys;
-            keyEventHandler = keyevent;
-            target.Focusable = true;
-            target.AddHandler(UIElement.PreviewKeyDownEvent, Receiver, true);
-            target.AddHandler(UIElement.PreviewKeyUpEvent, ReleaseReceiver, true);
-            target.AddHandler(UIElement.LostFocusEvent, ClearValues, true);
+            _keyEventHandler = handler;
+
+            if (TryGetTarget(out var element) && element is not null)
+            {
+                element.Focusable = true;
+                element.AddHandler(UIElement.PreviewKeyDownEvent, new KeyEventHandler(OnPreviewKeyDown), true);
+                element.AddHandler(UIElement.PreviewKeyUpEvent, new KeyEventHandler(OnPreviewKeyUp), true);
+                element.AddHandler(UIElement.LostKeyboardFocusEvent, new RoutedEventHandler(OnLostFocus), true);
+            }
         }
 
-        internal void Invoke(KeyEventArgs e)
+        private bool TryGetTarget(out UIElement? target) => _targetWeakRef.TryGetTarget(out target);
+
+        private void OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (TryGetTarget(out _))
+            {
+                _pressedKeys.Add(e.Key == Key.System ? e.SystemKey : e.Key);
+                CheckInvoke(e);
+            }
+        }
+        private void OnPreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if (TryGetTarget(out _))
+            {
+                _pressedKeys.Remove(e.Key == Key.System ? e.SystemKey : e.Key);
+                CheckInvoke(e);
+            }
+        }
+        private void OnLostFocus(object sender, RoutedEventArgs e) =>
+            _pressedKeys.Clear();
+        private void CheckInvoke(KeyEventArgs e)
         {
             if (_pressedKeys.IsSupersetOf(_targetKeys))
             {
-                keyEventHandler.Invoke(_target, e);
-                _pressedKeys.Clear();
+                if (TryGetTarget(out var target))
+                {
+                    _keyEventHandler(target, e);
+                    _pressedKeys.Clear();
+                }
             }
         }
-        internal void Receiver(object sender, KeyEventArgs e)
-        {
-            _pressedKeys.Add(e.Key == Key.System ? e.SystemKey : e.Key);
 
-            Invoke(e);
-        }
-        internal void ReleaseReceiver(object sender, KeyEventArgs e)
+        public void Dispose()
         {
-            _pressedKeys.Remove(e.Key == Key.System ? e.SystemKey : e.Key);
-
-            Invoke(e);
+            if (TryGetTarget(out var target) && target is not null)
+            {
+                target.RemoveHandler(UIElement.PreviewKeyDownEvent, (KeyEventHandler)OnPreviewKeyDown);
+                target.RemoveHandler(UIElement.PreviewKeyUpEvent, (KeyEventHandler)OnPreviewKeyUp);
+                target.RemoveHandler(UIElement.LostKeyboardFocusEvent, (RoutedEventHandler)OnLostFocus);
+            }
+            GC.SuppressFinalize(this);
         }
-        internal void ClearValues(object sender, RoutedEventArgs e)
-        {
-            _pressedKeys.Clear();
-        }
+        ~LocalHotKeyInjector() => Dispose();
     }
 }
